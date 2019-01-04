fd5d61b9ffc2332de3c4447a1f7694779bf519e4
Ahmad Al-Bayaty <ahmmab@gmail.com>
2019-01-04 16:38:13 -0500

    -Added Sidebar button click event. -Added responive overlay.

--- Template/wwwroot/js/app.js
+++ Template/wwwroot/js/app.js
@@ -4,9 +4,21 @@
 // Write your JavaScript code.
 
 
-//var navigation = new Vue({
-//    el: '#navigation',
-//    data: {
-//        message: 'Hello Vue!'
-//    }
-//});
+
+
+//Mobile Button. No Jquery required!
+var overlayClicked = document.getElementById('sidebar-overlay'); // or whatever triggers the toggle
+
+overlayClicked.addEventListener('click', function (e) {
+    e.preventDefault();
+    closeSidebar();
+});
+
+function openSidebar() {
+    document.getElementById("app").classList.add("sidebar-open");
+}
+
+
+function closeSidebar() {
+    document.getElementById("app").classList.remove("sidebar-open");
+}
